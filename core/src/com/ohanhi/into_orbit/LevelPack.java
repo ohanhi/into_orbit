b88d297606e90fe5cfce9ec1d597035a8dc29a68
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2014-11-04 15:17:40 +0200

    Make levels

--- core/src/com/ohanhi/into_orbit/LevelPack.java
+++ core/src/com/ohanhi/into_orbit/LevelPack.java
@@ -17,35 +17,55 @@ public class LevelPack {
         this.h = game.screenHeight;
         float radiusK = game.radiusK;
         this.levelPlanets = new Planet[][] {
-                { new Planet(0.5f * w, 0.5f * h, 1000, game) },
+                { new Planet(0.5f * w, 0.5f * h, 1000, game) }, // 1
                 { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) },
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 2
                 { new Planet(0.3f * w, 0.3f * h, 200, game),
                         new Planet(0.6f * w, 0.4f * h, 100, game),
-                        new Planet(0.5f * w, 0.65f * h, 400, game) },
+                        new Planet(0.5f * w, 0.65f * h, 400, game) }, // 3
 
-                { new Planet(0.5f * w, 0.5f * h, 1000, game) },
+                { new Planet(0.5f * w, 0.5f * h, 1000, game) }, // 4
                 { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) },
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 5
                 { new Planet(0.3f * w, 0.3f * h, 200, game),
                         new Planet(0.6f * w, 0.4f * h, 100, game),
-                        new Planet(0.5f * w, 0.65f * h, 400, game) }
+                        new Planet(0.5f * w, 0.65f * h, 400, game) }, // 6
+
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 7
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 8
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 9
         };
         this.levelGoals = new Goal[][] {
                 { new CircularGoal(0.5f * w, 0.3f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.7f * h, 30, 1, radiusK) },
+                        new CircularGoal(0.5f * w, 0.7f * h, 30, 1, radiusK) }, // 1
                 { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) },
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) }, // 2
                 { new CircularGoal(0.3f * w, 0.3f * h, 60, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK) },
+                        new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK) }, // 3
 
                 { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) },
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK),
+                        new CircularGoal(0.2f * w, 0.5f * h, 30, 1, radiusK)}, // 4
                 { new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
-                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) },
+                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // 5
                 { new CircularGoal(0.3f * w, 0.3f * h, 60, 1, radiusK),
                         new CircularGoal(0.6f * w, 0.4f * h, 40, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK)},
+                        new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK)}, // 6
+
+                { new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // 7
+                { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) }, // 8
+                { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK),
+                        new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // 9
         };
     }
 
