288dc471af80ab1d3c16c59f7d8c50b52c6286bb
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2014-12-21 19:32:14 +0200

    Add levels

--- core/src/com/ohanhi/into_orbit/LevelPack.java
+++ core/src/com/ohanhi/into_orbit/LevelPack.java
@@ -17,55 +17,111 @@ public class LevelPack {
         this.h = game.screenHeight;
         float radiusK = game.radiusK;
         this.levelPlanets = new Planet[][] {
-                { new Planet(0.5f * w, 0.5f * h, 1000, game) }, // 1
+
+                // single planet
+
+                { new Planet(0.5f * w, 0.5f * h, 1000, game) }, // simple
+                { new Planet(0.5f * w, 0.5f * h, 1000, game) }, // simple, 3 goals
+                { new Planet(0.5f * w, 0.5f * h, 3000, game) }, // ellipse
+
+
+                // two planets
+
                 { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 2
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // left-right
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // up-down
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // left-center-right
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // up-center-down
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // easy "fan"
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // harder fan
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // tricky fan
+                { new Planet(0.4f * w, 0.5f * h, 500, game),
+                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // four directions
+
+                // one planet, one moon
+
+                { new Planet(0.4f * w, 0.5f * h, 100, game),
+                        new Planet(0.5f * w, 0.5f * h, 1000, game) }, // x
+                { new Planet(0.4f * w, 0.5f * h, 100, game),
+                        new Planet(0.5f * w, 0.5f * h, 1000, game) }, // y
+
+                // three planets
+
                 { new Planet(0.3f * w, 0.3f * h, 200, game),
                         new Planet(0.6f * w, 0.4f * h, 100, game),
                         new Planet(0.5f * w, 0.65f * h, 400, game) }, // 3
-
-                { new Planet(0.5f * w, 0.5f * h, 1000, game) }, // 4
-                { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 5
                 { new Planet(0.3f * w, 0.3f * h, 200, game),
                         new Planet(0.6f * w, 0.4f * h, 100, game),
                         new Planet(0.5f * w, 0.65f * h, 400, game) }, // 6
 
-                { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 7
-                { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 8
-                { new Planet(0.4f * w, 0.5f * h, 500, game),
-                        new Planet(0.6f * w, 0.5f * h, 500, game) }, // 9
         };
         this.levelGoals = new Goal[][] {
+
+                // single planet
+
                 { new CircularGoal(0.5f * w, 0.3f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.7f * h, 30, 1, radiusK) }, // 1
-                { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) }, // 2
-                { new CircularGoal(0.3f * w, 0.3f * h, 60, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK) }, // 3
+                        new CircularGoal(0.5f * w, 0.7f * h, 30, 1, radiusK) }, // simple
+                { new CircularGoal(0.5f * w, 0.2f * h, 30, 2, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK),
+                        new CircularGoal(0.2f * w, 0.5f * h, 30, 1, radiusK)}, // simple, 3 goals
+                { new CircularGoal(0.1f * w, 0.5f * h, 30, 2, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 80, 2, radiusK) }, // ellipse
+
+
+                // two planets
 
                 { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK),
-                        new CircularGoal(0.2f * w, 0.5f * h, 30, 1, radiusK)}, // 4
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) }, // left-right
                 { new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
-                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // 5
+                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // up-down
+                { new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 30, 1, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // left-center-right
+                { new CircularGoal(0.5f * w, 0.2f * h, 30, 2, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 30, 4, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 2, radiusK) }, // up-center-down
+                { new CircularGoal(0.5f * w, 0.2f * h, 40, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 40, 5, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 40, 1, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 40, 1, radiusK) }, // easy "fan"
+                { new CircularGoal(0.5f * w, 0.2f * h, 40, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 40, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 40, 1, radiusK),
+                        new CircularGoal(0.65f * w, 0.3f * h, 40, 1, radiusK),
+                        new CircularGoal(0.65f * w, 0.7f * h, 40, 1, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 40, 1, radiusK) }, // harder fan
+                { new CircularGoal(0.5f * w, 0.2f * h, 40, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.5f * h, 40, 5, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 40, 1, radiusK),
+                        new CircularGoal(0.65f * w, 0.3f * h, 40, 1, radiusK),
+                        new CircularGoal(0.65f * w, 0.7f * h, 40, 1, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 40, 5, radiusK) }, // tricky fan
+                { new CircularGoal(0.5f * w, 0.2f * h, 30, 2, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 2, radiusK),
+                        new CircularGoal(0.3f * w, 0.5f * h, 30, 2, radiusK),
+                        new CircularGoal(0.7f * w, 0.5f * h, 30, 2, radiusK) }, // four directions
+
+                // one planet, one moon
+
+                { new CircularGoal(0.5f * w, 0.3f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.7f * h, 30, 1, radiusK) }, // x
+                { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK) }, // y
+
+                // three planets
+
+                { new CircularGoal(0.3f * w, 0.3f * h, 60, 1, radiusK),
+                        new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK) }, // 3
                 { new CircularGoal(0.3f * w, 0.3f * h, 60, 1, radiusK),
                         new CircularGoal(0.6f * w, 0.4f * h, 40, 1, radiusK),
                         new CircularGoal(0.5f * w, 0.65f * h, 100, 1, radiusK)}, // 6
 
-                { new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.5f * h, 30, 1, radiusK),
-                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // 7
-                { new CircularGoal(0.5f * w, 0.2f * h, 30, 2, radiusK),
-                        new CircularGoal(0.5f * w, 0.5f * h, 30, 4, radiusK),
-                        new CircularGoal(0.5f * w, 0.8f * h, 30, 2, radiusK) }, // 8
-                { new CircularGoal(0.5f * w, 0.2f * h, 30, 1, radiusK),
-                        new CircularGoal(0.5f * w, 0.5f * h, 30, 5, radiusK),
-                        new CircularGoal(0.5f * w, 0.8f * h, 30, 1, radiusK),
-                        new CircularGoal(0.3f * w, 0.5f * h, 30, 1, radiusK),
-                        new CircularGoal(0.7f * w, 0.5f * h, 30, 1, radiusK) }, // 9
         };
     }
 
